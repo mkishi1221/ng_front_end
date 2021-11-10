2e70b7ad0b8829b39271ef2b55e06f7b7b41f1ed
liquidiert <haberederkorbinian@googlemail.com>
2021-11-10 16:26:59 +0100

    init commit
      - added init nuxt project and default layouts

---
+++ layouts/default.vue
@@ -0,0 +1,34 @@
+<template>
+    <nuxt />
+</template>
+
+<script lang='ts'>
+import {Component, Prop, Vue} from 'nuxt-property-decorator';
+
+@Component({
+    name: 'DefaultLayout',
+    components: {}
+})
+export default class DefaultLayout extends Vue {
+    // Props
+
+    // Data
+
+    // Hook Callbacks
+
+    // Refs
+
+    // Getters
+
+    // Setters
+
+    // Watchers
+
+    // Logic
+
+}
+</script>
+
+<style scoped>
+
+</style>
