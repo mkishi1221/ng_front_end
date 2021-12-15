ed55084f73ba551864070089d1a06d138ffabd3f
Masayuki Kishi <masayukikishi@Masayukis-MacBook-Pro.local>
2021-12-15 19:20:21 +0100

    updated to tailwind 3.0 and updated css

--- components/t-section.vue
+++ components/t-section.vue
@@ -1,14 +1,13 @@
 <template>
   <div class="container mx-auto px-14 xl:px-6 pt-2">
     <div class="flex justify-between items-center">
-      <span class="text-xl text-black text-opacity-50 dark:text-white font-bold">{{ title }}</span>
+      <span class="text-xl text-black text-opacity-50 dark:text-white font-bold align-bottom">{{ title }}</span>
       <div class="relative inline-block">
         <t-menu>
           <template v-slot:activator>
             <t-icon
               icon="mdi-dots-horizontal"
-              class="cursor-pointer dark:text-white "
-              size="6"
+              class="cursor-pointer dark:text-white text-2xl "
             />
           </template>
           <slot name="menu-entries" />
